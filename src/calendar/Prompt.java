58d2cfa8a7615b76635c0ad158a061f23e8d8aa3
Bockya <rhk3562@naver.com>
2020-11-22 20:00:42 +0900

    add printCalendar()

--- src/calendar/Prompt.java
+++ src/calendar/Prompt.java
@@ -9,14 +9,18 @@ public class Prompt {
         //숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
         Scanner sc = new Scanner(System.in);
         Calendar c = new Calendar();
+        int year = 2020;
         int month = 1;
         while (true) {
+            System.out.println("년도를 입력하세요");
+            System.out.print("YEAR> ");
+            year = sc.nextInt();
             System.out.println("달을 입력하세요");
-            System.out.print(PROMPT);
+            System.out.print("MONTH> ");
             month = sc.nextInt();
             if (month == -1) break;
             if (month > 12) continue;
-            c.printCalendar(2020, month);
+            c.printCalendar(year, month);
         }
         System.out.println("Bye~");
         sc.close();
