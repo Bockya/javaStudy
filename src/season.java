a7dc1401f6c2c72e68889a56ec9ceabd0c4fae86
Bockya <rhk3562@naver.com>
2021-01-14 01:47:38 +0900

    2단계 : enhanced switch

--- src/season.java
+++ src/season.java
@@ -4,18 +4,11 @@ class season {
     public static void main(String[] args) {
         Random r = new Random();
         int month = r.nextInt(12) + 1;
-        switch (month){
-            case 3: case 4: case 5:
-                System.out.println("봄입니다");
-                break;
-            case 6: case 7: case 8:
-                System.out.println("여름입니다");
-                break;
-            case 9: case 10: case 11:
-                System.out.println("가을입니다");
-                break;
-            case 12: case 1: case 2:
-                System.out.println("겨울입니다");
+        switch (month) {
+            case 3, 4, 5 -> System.out.println("봄입니다");
+            case 6, 7, 8 -> System.out.println("여름입니다");
+            case 9, 10, 11 -> System.out.println("가을입니다");
+            case 12, 1, 2 -> System.out.println("겨울입니다");
         }
     }
 }
