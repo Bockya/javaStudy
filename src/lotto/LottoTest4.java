0275a26fb2001cacc89ac70a5319872b1e3249f4
Bockya <rhk3562@naver.com>
2021-01-17 00:31:54 +0900

    4단계 : 셔플 활용

---
+++ src/lotto/LottoTest4.java
@@ -0,0 +1,20 @@
+package lotto;
+
+import java.util.Arrays;
+import java.util.Random;
+
+class LottoTest4 {
+    public static void main(String[] args) {
+        int[] ball = new int[45];
+        for (int i = 0; i < ball.length; i++)
+            ball[i] = i + 1;
+        Random r = new Random();
+        for (int i = 0; i < 1000; i++) {
+            int idx = r.nextInt(ball.length);
+            int tmp = ball[0];
+            ball[0] = ball[idx];
+            ball[idx] = tmp;
+        }
+        System.out.println(Arrays.toString(Arrays.copyOfRange(ball, 0, 6)));
+    }
+}
