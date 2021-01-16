261707e648a996bcc00234cf52d0ecf5f2954c0a
Bockya <rhk3562@naver.com>
2021-01-16 23:51:42 +0900

    1단계 : 중복된 숫자 방지하고 로또 6개 생성

---
+++ src/lotto/LottoTest1.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class LottoTest1 {
+    public static void main(String[] args) {
+        final int MAX_NUMBER = 45;
+        int[] numbers = new int[MAX_NUMBER];
+
+        // 1 ~ 45의 숫자를 생성한 수 배열에 입력
+        for (int i = 0; i < MAX_NUMBER; i++)
+            numbers[i] = i + 1;
+
+        Random r = new Random();
+        int[] lottoNums = new int[6];
+        for (int i = 0; i < 6; i++) {
+            int seq = r.nextInt(MAX_NUMBER - i);
+            // 생성된 숫자를 결과 배열에 저장
+            lottoNums[i] = numbers[seq];
+            // 배열에 저장된 숫자 위치 변경
+            numbers[seq] = numbers[MAX_NUMBER - i - 1];
+        }
+        System.out.println(Arrays.toString(lottoNums));
+    }
+}
